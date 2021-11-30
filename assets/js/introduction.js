f9c292b3edf517d4f26db452525c0dcd0f73818f
Ewelina <ewelina@ekopacz.pl>
2021-11-30 21:17:50 +0100

    add part of solution

---
+++ assets/js/introduction.js
@@ -0,0 +1,35 @@
+
+const toolTipElements = document.querySelectorAll('.tooltip');
+
+
+if(toolTipElements){
+    toolTipElements.forEach(function(element){
+        //utworzenie elementów
+        const aElement = document.createElement('a');
+        const spanElement = document.createElement('span');
+        const imgElement = document.createElement('img');
+
+        if(element.dataset.tooltipType === 'image'){
+            spanElement.setAttribute('class','tooltip__box tool tip__box--image');
+            spanElement.appendChild(imgElement);
+            imgElement.setAttribute('src',element.dataset.tooltipContent);
+        }
+        else{
+            spanElement.setAttribute('class','tooltip__box tooltip__box--text');
+            spanElement.innerHTML = element.dataset.tooltipContent;
+        }
+
+        //przypisanie atrybutów
+        aElement.setAttribute('href',element.dataset.url);
+
+        //usunięcie text i dodanie go do znacznika <a></a> jako anchor text
+        const anchorText = element.innerText;
+        element.innerText = '';
+        aElement.innerText = anchorText;
+
+        // wstawienie dzieci
+        element.appendChild(aElement);
+        element.appendChild(spanElement);
+    });
+
+}
