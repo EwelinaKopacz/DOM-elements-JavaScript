4b5dd5caf5bcba667da882984331e3af4ed7c578
Ewelina <ewelina@ekopacz.pl>
2021-12-06 15:30:32 +0100

    upgrade

--- assets/js/introduction.js
+++ assets/js/introduction.js
@@ -1,13 +1,36 @@
 
 const toolTipElements = document.querySelectorAll('.tooltip');
 
+function createLinkElement(itemA,itemURL) {
+    itemA = document.createElement('a');
+    itemA.setAttribute('href',itemURL);
+    return itemA;
+}
+
+function createSpanElement(itemSpan) {
+    itemSpan = document.createElement('span');
+    return itemSpan;
+    // if(itemType === 'image'){
+    //     return itemSpan.setAttribute('class','tooltip__box tool tip__box--image');
+
+    // }
+    // else {
+    //     return itemSpan.setAttribute('class','tooltip__box tooltip__box--text');
+    // }
+}
+
+function createImgElement(itemImg) {
+    itemImg = document.createElement('img');
+    return itemImg;
+}
+
 
 if(toolTipElements){
     toolTipElements.forEach(function(element){
         //utworzenie elementów
-        const aElement = document.createElement('a');
-        const spanElement = document.createElement('span');
-        const imgElement = document.createElement('img');
+        const aElement = createLinkElement(element,element.dataset.url);
+        const spanElement = createSpanElement(element);
+        const imgElement = createImgElement(element);
 
         //przypisanie atrybutów
         if(element.dataset.tooltipType === 'image'){
@@ -20,8 +43,6 @@ if(toolTipElements){
             spanElement.innerHTML = element.dataset.tooltipContent;
         }
 
-        aElement.setAttribute('href',element.dataset.url);
-
         //usunięcie text i dodanie go do znacznika <a></a> jako anchor text
         const anchorText = element.innerText;
         element.innerText = '';
@@ -33,3 +54,42 @@ if(toolTipElements){
     });
 
 }
+
+
+
+// PONIZEJ KOPIA ZOSTAWIC 
+
+// if(toolTipElements){
+//     toolTipElements.forEach(function(element){
+//         //utworzenie elementów
+//         const aElement = createLinkElement(element,element.dataset.url);
+//         const spanElement = createSpanElement(element,element.dataset.tooltipType);
+//         const imgElement = createImgElement(element);
+
+//         // const aElement = document.createElement('a'); // do usuniecia
+//         // const spanElement = document.createElement('span'); // do usuniecia
+//         // const imgElement = document.createElement('img'); // do usuniecia 
+
+//         //przypisanie atrybutów
+//         if(element.dataset.tooltipType === 'image'){
+//             spanElement.setAttribute('class','tooltip__box tool tip__box--image');
+//             spanElement.appendChild(imgElement);
+//             imgElement.setAttribute('src',element.dataset.tooltipContent);
+//         }
+//         else{
+//             spanElement.setAttribute('class','tooltip__box tooltip__box--text');
+//             spanElement.innerHTML = element.dataset.tooltipContent;
+//         }
+
+//         // aElement.setAttribute('href',element.dataset.url);  // do usuniecia 
+
+//         //usunięcie text i dodanie go do znacznika <a></a> jako anchor text
+//         const anchorText = element.innerText;
+//         element.innerText = '';
+//         aElement.innerText = anchorText;
+
+//         // wstawienie dzieci
+//         element.appendChild(aElement);
+//         element.appendChild(spanElement);
+//     });
+// }
