425a9c9320133842478c21e6a450c4ae6bd60d68
Ewelina <ewelina@ekopacz.pl>
2021-12-07 08:39:52 +0100

    add functions

--- assets/js/introduction.js
+++ assets/js/introduction.js
@@ -7,89 +7,42 @@ function createLinkElement(itemA,itemURL) {
     return itemA;
 }
 
-function createSpanElement(itemSpan) {
+function createSpanElement(itemSpan, itemSpanType, itemSpanContent) {
     itemSpan = document.createElement('span');
-    return itemSpan;
-    // if(itemType === 'image'){
-    //     return itemSpan.setAttribute('class','tooltip__box tool tip__box--image');
-
-    // }
-    // else {
-    //     return itemSpan.setAttribute('class','tooltip__box tooltip__box--text');
-    // }
+    if(itemSpanType === 'text'){
+        itemSpan.setAttribute('class','tooltip__box tool tip__box--text');
+        itemSpan.innerHTML = itemSpanContent;
+        return itemSpan;
+    }
+    else {
+        itemSpan.setAttribute('class','tooltip__box tooltip__box--image');
+        return itemSpan;
+    }
 }
 
-function createImgElement(itemImg) {
+function createImgElement(itemImg, ItemImgContent) {
     itemImg = document.createElement('img');
+    itemImg.setAttribute('src',ItemImgContent)
     return itemImg;
 }
 
+function addText(textToChange,aElement){
+    aElement.innerText = textToChange;
+}
 
 if(toolTipElements){
     toolTipElements.forEach(function(element){
-        //utworzenie elementów
         const aElement = createLinkElement(element,element.dataset.url);
-        const spanElement = createSpanElement(element);
-        const imgElement = createImgElement(element);
+        const spanElement = createSpanElement(element,element.dataset.tooltipType,element.dataset.tooltipContent);
 
-        //przypisanie atrybutów
-        if(element.dataset.tooltipType === 'image'){
-            spanElement.setAttribute('class','tooltip__box tool tip__box--image');
-            spanElement.appendChild(imgElement);
-            imgElement.setAttribute('src',element.dataset.tooltipContent);
-        }
-        else{
-            spanElement.setAttribute('class','tooltip__box tooltip__box--text');
-            spanElement.innerHTML = element.dataset.tooltipContent;
-        }
-
-        //usunięcie text i dodanie go do znacznika <a></a> jako anchor text
-        const anchorText = element.innerText;
+        addText(element.innerText,aElement);
         element.innerText = '';
-        aElement.innerText = anchorText;
 
-        // wstawienie dzieci
-        element.appendChild(aElement);
-        element.appendChild(spanElement);
+        if(element.dataset.tooltipType === 'image'){
+            const imgElement = createImgElement(element,element.dataset.tooltipContent);
+            spanElement.appendChild(imgElement);
+        }
+            element.appendChild(aElement);
+            element.appendChild(spanElement);
     });
-
 }
-
-
-
-// PONIZEJ KOPIA ZOSTAWIC 
-
-// if(toolTipElements){
-//     toolTipElements.forEach(function(element){
-//         //utworzenie elementów
-//         const aElement = createLinkElement(element,element.dataset.url);
-//         const spanElement = createSpanElement(element,element.dataset.tooltipType);
-//         const imgElement = createImgElement(element);
-
-//         // const aElement = document.createElement('a'); // do usuniecia
-//         // const spanElement = document.createElement('span'); // do usuniecia
-//         // const imgElement = document.createElement('img'); // do usuniecia 
-
-//         //przypisanie atrybutów
-//         if(element.dataset.tooltipType === 'image'){
-//             spanElement.setAttribute('class','tooltip__box tool tip__box--image');
-//             spanElement.appendChild(imgElement);
-//             imgElement.setAttribute('src',element.dataset.tooltipContent);
-//         }
-//         else{
-//             spanElement.setAttribute('class','tooltip__box tooltip__box--text');
-//             spanElement.innerHTML = element.dataset.tooltipContent;
-//         }
-
-//         // aElement.setAttribute('href',element.dataset.url);  // do usuniecia 
-
-//         //usunięcie text i dodanie go do znacznika <a></a> jako anchor text
-//         const anchorText = element.innerText;
-//         element.innerText = '';
-//         aElement.innerText = anchorText;
-
-//         // wstawienie dzieci
-//         element.appendChild(aElement);
-//         element.appendChild(spanElement);
-//     });
-// }
